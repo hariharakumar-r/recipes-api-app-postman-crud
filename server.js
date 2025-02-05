5fc0fd8d34dc3f4c61ccebd9230af56c97ee8da8
Hariharakumar Ranganathan <hariharakumar08@gmail.com>
2025-02-05 17:22:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 //Routes
-app.use('/api/recipes', recipeRoutes);
+app.use('/api/recipe', recipeRoutes);
 
 //Connect to MongoDBegijerogij
 mongoose.connect(process.env.MONGO_URI)
